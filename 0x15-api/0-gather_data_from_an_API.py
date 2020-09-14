8c33971df08b55f9ba15ae2fc33ef46732d4a0f3
KoeusIss <sebri.issam@gmail.com>
2020-09-14 14:05:18 +0100

    commit the first taské

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Returns the list of TODOS
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    empl_id = sys.argv[1]
+    r1 = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                     format(empl_id))
+    empl_name = r1.json()['name']
+    r2 = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                     format(empl_id))
+    responses = r2.json() 
+    done_tasks = [t for t in responses if t.get('completed')]
+    print('Employee {} is done with tasks({}/{}):'.
+          format(empl_name, len(done_tasks), len(responses)))
+    for t in done_tasks:
+        print('\t{}'.format(t.get('title')))
