c7fd1caf7ea1cd79d67c72d0d281582b02a0f61a
KoeusIss <sebri.issam@gmail.com>
2020-09-14 18:04:12 +0100

    fix pep8 and bind the url_api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-# Exports tasks to csv
+"""DataAPI module"""
 import csv
 import requests
 import sys
 
 
 if __name__ == '__main__':
-    empl_id = sys.argv[1]
-    r1 = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                     format(empl_id))
-    empl_username = r1.json().get('username')
-    r2 = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
-                     format(empl_id))
-    responses = r2.json() 
-    with open('{}.csv'.format(empl_id), 'w') as file:
+    api_url = 'https://jsonplaceholder.typicode.com'
+    employee_id = sys.argv[1]
+    r1 = requests.get('{}/users/{}'.format(api_url, employee_id))
+    employee_username = r1.json().get('username')
+    r2 = requests.get('{}/todos?userId={}'.format(api_url, employee_id))
+    responses = r2.json()
+    with open('{}.csv'.format(employee_id), 'w') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in responses:
             writer.writerow([
-                empl_id,
-                empl_username,
+                employee_id,
+                employee_username,
                 task.get('completed'),
                 task.get('title')
             ])
