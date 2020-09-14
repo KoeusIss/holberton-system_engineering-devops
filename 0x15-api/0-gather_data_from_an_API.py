9d50876117fd4890277e55493ca7e689a5f9517d
KoeusIss <sebri.issam@gmail.com>
2020-09-14 17:56:14 +0100

    fix the pep8 style, add a space before the task title

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,15 +5,14 @@ import sys
 
 
 if __name__ == '__main__':
-    empl_id = sys.argv[1]
-    r1 = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                     format(empl_id))
-    empl_name = r1.json()['name']
-    r2 = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
-                     format(empl_id))
-    responses = r2.json() 
+    api_url = 'https://jsonplaceholder.typicode.com'
+    employee_id = sys.argv[1]
+    r1 = requests.get('{}/users/{}'.format(api_url, employee_id))
+    employee_name = r1.json().get('name')
+    r2 = requests.get('{}/todos?userId={}'.format(api_url, employee_id))
+    responses = r2.json()
     done_tasks = [t for t in responses if t.get('completed')]
     print('Employee {} is done with tasks({}/{}):'.
-          format(empl_name, len(done_tasks), len(responses)))
+          format(employee_name, len(done_tasks), len(responses)))
     for t in done_tasks:
-        print('\t{}'.format(t.get('title')))
+        print('\t {}'.format(t.get('title')))
