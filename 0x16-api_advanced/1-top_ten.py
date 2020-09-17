2ee4f1d05e6887bb5849f42ebe7519af59d2655b
KoeusIss <sebri.issam@gmail.com>
2020-09-17 19:48:09 +0100

    fix the params

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,20 +6,22 @@ import requests
 def top_ten(subreddit):
     """Returns the top 10 hotest posts for a given subreddit"""
     base_url = 'https://www.reddit.com'
-    query = 'r/{}/hot.json?limit=10'.format(subreddit)
+    query = 'r/{}/hot.json'.format(subreddit)
     headers = {
         "User-Agent": "linux:hbtn.advanced.api (by /u/koeusiss)"
+    }
+    params = {
+        "limit": 10
     }
     req = requests.get(
         url='{}/{}'.format(base_url, query),
         headers=headers,
+        params=params,
         allow_redirects=False
     )
     if req.status_code == 404:
         print('None')
+        return
     res = req.json().get('data').get('children')
     for children in res:
         print(children.get('data').get('title'))
-
-if __name__ == '__main__':
-    top_ten(subreddit)
